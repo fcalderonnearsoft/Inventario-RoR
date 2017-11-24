c0a808a15da9586f8ebcb2f9445fa28165c051de
fcalderon <fcalderon@nearsoft.com>
2017-11-24 12:54:33 -0700

    remove unused resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :equipments
   resources :locations
-  resources :equipment_in_locations
   root 'equipments#index'
 end
