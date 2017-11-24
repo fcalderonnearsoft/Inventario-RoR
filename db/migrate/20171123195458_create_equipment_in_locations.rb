a6bdf6e4c668bcb86439205f597ae87773b86589
fcalderon <fcalderon@nearsoft.com>
2017-11-23 23:02:14 -0700

    update models

--- db/migrate/20171123195458_create_equipment_in_locations.rb
+++
@@ -1,8 +0,0 @@
-class CreateEquipmentInLocations < ActiveRecord::Migration[5.1]
-  def change
-    create_table :equipment_in_locations do |t|
-      t.integer :quantity
-      t.timestamps
-    end
-  end
-end
